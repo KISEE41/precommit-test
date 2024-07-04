3994e630e0c2b376103a2747050d5a375cf4ac75
kisee41 <shreejan.kisee005@gmail.com>
2024-07-04 13:46:36 +0545

    initial commit

---
+++ pre_commit_hook/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from .check_notebook import main
+
+__all__ = [
+    main,
+]
---
+++ pre_commit_hook/check_notebook.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from loguru import logger
+
+
+def main() -> None:
+    logger.info('precommit working!!!!!!!!!')
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
